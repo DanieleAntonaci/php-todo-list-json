1a86c2b57d826d0c167cc438a157caa067dea166
DanieleAntonaci <danieleanto96@gmail.com>
2023-01-12 17:04:29 +0100

    Creazione cartella

---
+++ Client/src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import ToDo from './components/ToDo.vue';
+export default {
+  components: { ToDo },
+  data() {
+    return {};
+  },
+}
+</script>
+
+<template>
+  <ToDo/>
+</template>
+
+<style lang=" scss">
+
+</style>
---
+++ Client/src/components/ToDo.vue
@@ -0,0 +1,37 @@
+<script >
+import axios from 'axios';
+
+
+export default {
+    data() {
+        return {
+            apiUrl: "http://localhost:8888/",
+            todoList: [],
+
+        }
+    },
+    methods: {
+        getData() {
+            axios.get(this.apiUrl + "api.php")
+                .then(res => {
+
+                    const data = res.data;
+
+                    this.todoList = data;
+                });
+
+        }
+    },
+    mounted() {
+        this.getData();
+    }
+}
+</script>
+
+<template>
+  gg
+</template>
+
+<style lang="scss" scoped>
+
+</style>
